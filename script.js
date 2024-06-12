4b274bb2b689eff2cad756b868cbde12efb53993
MustafaRaheel26 <MY_NAME@example.com>
2024-06-11 18:15:37 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("login-username").value;
+    var password = document.getElementById("login-password").value;
+    var storedUser = localStorage.getItem(username);
+    if (storedUser && JSON.parse(storedUser).password === password) {
+      alert("Login successful!");
+    } else {
+      alert("Invalid username or password!");
+    }
+  });
+  
+  document.getElementById("signup-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("signup-username").value;
+    var password = document.getElementById("signup-password").value;
+    var user = { username: username, password: password };
+    localStorage.setItem(username, JSON.stringify(user));
+    alert("Signup successful!");
+  });
+  
